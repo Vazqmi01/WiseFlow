030468147c140793efb09de03d3240a449a1f393
Vazqmi01 <migvazgon@gmail.com>
2025-11-06 20:32:43 -0600

    V1

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
 import streamlit as st
 
-st.title("📊 FinguIA")
-st.caption("💰 Inversiones simplificadas.")
-
-prompt = st.chat_input("En qué te puedo ayudar?")
-if prompt:
-    st.write(f"El usuario ha enviado el siguiente prompt: '{prompt}'")
+st.title('Mi Primera App WiseFlow')
+st.write('¡Streamlit funciona correctamente!')
